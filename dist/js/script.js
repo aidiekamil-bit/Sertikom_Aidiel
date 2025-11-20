8ab9a8880308d66696e0bde0731fe80b0b0cbcdc
aidiekamil-bit <aidiekamil@gmail.com>
2025-11-20 15:24:41 +0700

    pembaruan

---
+++ dist/js/script.js
@@ -0,0 +1,5 @@
+const navBar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  const posisi = window.scrollY > 0;
+  navBar.classList.toggle("scrolling-active", posisi);
+});
